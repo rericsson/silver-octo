7eb0987fe4c68603d9003f23510e14a172074acc
Rob Ericsson <rericsson@vestapartners.com>
2020-05-29 17:17:14 -0400

    fixed typo in setup

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
         install_requires=[
             "Click",
             "dataclasses-json",
+            "python-dotenv",
             "openpyxl",
             "requests",
             "requests-oauthlib",
